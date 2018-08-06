c326ee8fa4711e0860413609b1afa7274e6cb985
KarimM7mad <KarimMohamed95@hotmail.com>
2018-08-06 08:32:58 +0200

    all done ,with files handling

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <bitset>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 #define numOfRounds 8
@@ -19,6 +20,23 @@ int generateRandomInteger(int max) {
     for(int i = 0; i < 100; i++)
         x = (rand() % max + 1);
     return x;
+}
+
+string getFromFile(string filename="input.txt") {
+    string x;
+    char tmp;
+    ifstream infile(filename.c_str());
+    while(!infile.eof()) {
+        infile.get(tmp);
+        x+=tmp;
+    }
+    return x.substr(0,x.size()-2);
+}
+
+void saveToFile(string data, string filename) {
+    ofstream outfile(filename.c_str());
+    outfile << data + "";
+    outfile.close();
 }
 
 class fiestel {
@@ -36,11 +54,8 @@ public:
     fiestel(string dataBlock, string keyBlock) {
         this->dataBlock = dataBlock;
         this->keyBlock = keyBlock;
-        cout << "-------------Process Started-------------------" << endl;
         //generate round keys
-        cout << "-------------Keys generation Started-------------------" << endl;
         this->generateKeys();
-        cout << "-------------Keys generation Ended-------------------" << endl;
     }
 
     void execute(string value, string key, bool modeToUse) {
@@ -56,19 +71,13 @@ public:
         string LEnext, REnext, LEcurr, REcurr, tmp;
         LEcurr = this->dataBlock.substr(0, 4);
         REcurr = this->dataBlock.substr(4, 4);
-
         if(b) {
-            cout << "-------------Start Encryption-------------------------" << endl;
             for(int i = 0; i < numOfRounds; i++) {
                 LEnext = REcurr;
-                cout << "LEnext|" + LEnext + "|" << endl;
-                cout << "key|" + (this->finalKeys[i]) + "|" << endl;
                 tmp = functionF(REcurr, this->finalKeys[i]);
                 for(int ii = 0; ii < 4; ii++) {
                     REnext.append(1, tmp[ii] ^ LEcurr[ii]);
                 }
-                cout << "REnext|" + REnext + "|" << endl;
-                cout << "---------------------------------------------------------" << endl;
                 //clear all that will be used in order to use them for the other loops
                 tmp = "";
                 LEcurr = LEnext;
@@ -80,24 +89,14 @@ public:
             tmp = LEcurr;
             LEcurr = REcurr;
             REcurr = tmp;
-            cout << "--------------------------End Encryption-------------------------------" << endl;
-            cout << "cipher is " + (LEcurr + REcurr) << endl;
-            cout << "---------------------------------------------------------" << endl;
         }
-
-
         else if(!b) {
-            cout << "-------------Start Decryption-------------------------" << endl;
             for(int i = 7; i >= 0; i--) {
                 LEnext = REcurr;
-                cout << "LEnext|" + LEnext + "|" << endl;
-                cout << "key|" + (this->finalKeys[i]) + "|" << endl;
                 tmp = functionF(REcurr, this->finalKeys[i]);
                 for(int ii = 0; ii < 4; ii++) {
                     REnext.append(1, tmp[ii] ^ LEcurr[ii]);
                 }
-                cout << "REnext|" + REnext + "|" << endl;
-                cout << "---------------------------------------------------------" << endl;
                 //clear all that will be used in order to use them for the other loops
                 tmp = "";
                 LEcurr = LEnext;
@@ -109,9 +108,6 @@ public:
             tmp = LEcurr;
             LEcurr = REcurr;
             REcurr = tmp;
-            cout << "--------------------------End Decryption-------------------------------" << endl;
-            cout << "Original is " + (LEcurr + REcurr) << endl;
-            cout << "---------------------------------------------------------" << endl;
         }
         this->result = LEcurr + REcurr;
         cout << "RESULT is " + this->result << endl;
@@ -122,7 +118,6 @@ public:
         for(int i = 0; i < 4; i++) {
             s.append(1, RE[i]^keyy[i]);
         }
-        cout << "stringInF|" + (s) + "|" << endl;
         return s;
     }
     //random keys generator
@@ -154,44 +149,73 @@ public:
         for(int i = 0; i < numOfRounds; i++)
             this->finalKeys[i] = keys[i].substr(2, 4);
     }
-
 };
 
-string getTxtFromFile(string filename="input.txt"){
-    string x;
-    char tmp;
-    ifstream infile(filename);
-    while( !infile.eof() ){
-        infile.get(tmp);
-        x.append(1,tmp);
-    }
-    infile.close();
-    return x;
-}
-
-void saveToFile(string data,string filename="output.txt"){
-    ofstream outfile(filename);
-    outfile << data;
-    outfile.close();
-}
-
 int main() {
-
-
-    //    char dataEntered[9], keyEntered[9];
-    //
-    //    cout << "enter data (max 8 chars)" << endl;
-    //    cin.get(dataEntered, 9);
-    //    
-    //    cin.get();
-    //    
-    //    cout << "enter key (max 8 chars)" << endl;
-    //    cin.get(keyEntered, 9);
-    //    
-    //    fiestel f(dataEntered, keyEntered);
+ 
+    //get file content without new line
+    string rawFileData = getFromFile();    
+    int remaining = rawFileData.size()%8 ;
+    remaining = remaining*8 -rawFileData.size();
+    //in case the remainder is negative
+    while(remaining<0)
+        remaining+=8;
+    for(int i=0;i<remaining;i++){
+        rawFileData.append(1,'.');
+    }
+    //divided it's content into blocks ( 8 chars each )
+    int sizee = rawFileData.size()/8;
+    string inputs[sizee];
+    fiestel agents[sizee];
+    //creating cipher blocks for each data block
+    for(int i=0;i<sizee;i++){
+        inputs[i]=rawFileData.substr(i*8,8);
+        fiestel tmp(inputs[i],"abcdefgh");
+        agents[i] = tmp;
+    }
+    
+    string encryptedFileContent="";
+    
+    for(int i =0;i<sizee;i++){
+        agents[i].execute(true);
+        encryptedFileContent+=agents[i].result;
+    }
+    
+    saveToFile(encryptedFileContent,"firstTrial.txt");
     
     
+    // preview on Decryption
+    string encryptedCode = getFromFile("firstTrial.txt"); 
+    int remaining2 = encryptedCode.size()%8 ;
+    remaining2 = remaining2*8 -encryptedCode.size();
+    //in case the remainder is negative
+    while(remaining2<0)
+        remaining2+=8;
+    for(int i=0;i<remaining2;i++){
+        encryptedCode.append(1,'.');
+    }
+    //divided it's content into blocks ( 8 chars each )
+    int sizee2 = encryptedCode.size()/8;
+    sizee2-=1;
+    cout<<sizee2<<endl;
+    string inputss[sizee2];
     
+    // this time i will use the same agents in order to have the keys 
+    // from them as i use time as a seed which will never be repeatable 
+    
+    for(int i=0;i<sizee2;i++){
+        inputss[i]=encryptedCode.substr(i*8,8);
+        agents[i].dataBlock=inputss[i];
+        agents[i].result="";
+    }
+    
+    string decryptedFileContent ="";
+    
+    for(int i=0;i<sizee2;i++){
+        agents[i].execute(false);
+        decryptedFileContent+=agents[i].result;
+    }
+    
+    saveToFile(decryptedFileContent,"finalOutput.txt");
     return 0;
 }
-
